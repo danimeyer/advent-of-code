56a890716d03206da2cc635ca110059d191684fa
Dani Meyer <danimeyer@github.com>
2022-12-04 10:55:26 -0500

    Switched back to original code but combined iterators

--- 2022/4.rb
+++ 2022/4.rb
@@ -4,29 +4,30 @@ input.map! do |pair|
   pair.split(/\D/).map! { |c| c.to_i }
 end
 
-def inclusive_pair_count(arr)
-  arr.reduce(0) do |sum, pair|
-    if (pair[2]..pair[3]).cover?(pair[0]) && (pair[2]..pair[3]).cover?(pair[1])
-      sum += 1
-    elsif (pair[0]..pair[1]).cover?(pair[2]) && (pair[0]..pair[1]).cover?(pair[3])
-      sum += 1
-    else
-      sum
-    end
+def in_range(num, range)
+  if num >= range[0] && num <= range[1]
+    return true
+  else 
+    return false
   end
 end
 
-def overlapping_pair_count(arr)
-  arr.reduce(0) do |sum, pair|
-    if (pair[2]..pair[3]).cover?(pair[0]) || (pair[2]..pair[3]).cover?(pair[1])
-      sum += 1
-    elsif (pair[0]..pair[1]).cover?(pair[2]) || (pair[0]..pair[1]).cover?(pair[3])
-      sum += 1
-    else
-      sum
-    end
+inclusive_count = 0
+overlapping_count = 0
+
+input.each do |pair|
+  if in_range(pair[0], pair[2..3]) && in_range(pair[1], pair[2..3])
+    inclusive_count += 1
+  elsif in_range(pair[2], pair[0..1]) && in_range(pair[3], pair[0..1])
+    inclusive_count += 1
+  end
+
+  if in_range(pair[0], pair[2..3]) || in_range(pair[1], pair[2..3])
+    overlapping_count += 1
+  elsif in_range(pair[2], pair[0..1]) || in_range(pair[3], pair[0..1])
+    overlapping_count += 1
   end
 end
 
-puts "inclusive pair count", inclusive_pair_count(input)
-puts "overlapping pair count", overlapping_pair_count(input)
+puts "inclusive pair count: #{inclusive_count}"
+puts "overlapping pair count: #{overlapping_count}"
--- 2022/Thoughts.md
+++ 2022/Thoughts.md
@@ -7,3 +7,7 @@ A lot of things have changed since last year, though! I'm getting more and more 
 While I work with Ruby and Rails on a regular basis at work, I still don't really feel like I know the ins and outs of the language the way I do with JavaScript, which is the first language I learned and probably the one I still know the best.
 
 So my goal for Advent of Code is to have fun working through the problems and improve my Ruby! I'm not sure if I'll complete the whole thing or not, but during these first few days, I'm enjoying working through puzzles and learning new methods and approaches to solving these problems. One of the things I love most about coding is all the different ways to reach the same solution, so I'm especially enjoying seeing how other people are solving this problems and improving my own code as a result.
+
+## Day Four
+I am enjoying this immensely so far, but good lord does Ruby have a lot of methods! Sometimes I stumble across them when looking up how to do something, but a lot of the time I discover they exist when reading other people's solutions. Yesterday I remembered the `reduce` method exists, while today I learned about `cover`! 
+Today it's the `count` and `cover` methods, which really helped condense my code while still keeping it readable!
