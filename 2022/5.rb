b07c081b9fbe430105a75d8c993bcbb358738d1e
Dani Meyer <danimeyer@github.com>
2022-12-06 13:54:11 -0500

    Refactored day five

--- 2022/5.rb
+++ 2022/5.rb
@@ -2,46 +2,67 @@ input = File.read("./inputs/day-five-input.txt").split("\n\n")
 
 stack_input = input[0].split("\n")
 stack_key = stack_input.pop
-stacks = {}
-
-stack_key.each_char.with_index do |c, idx|
-  if c != " "
-    key = c.to_i
-    stacks[key] = []
-    stack_input.each do |row|
-      if row[idx] != " "
-        stacks[key].push(row[idx])
-      end
-    end
-    stacks[key].reverse!
-  end
-end
 
 moves_input = input[1].split("\n").map! do |line| 
   line.split(/\D/).reject(&:empty?).map(&:to_i)
 end
 
-moves_input.each do |move|
-  count = move[0]
-  start = move[1]
-  finish = move[2]
+def create_stacks(keys, containers)
+  stacks = {}
 
-  # Comment lines 30-31 in and 34-38 out for solution 2
-  # temp = stacks[start].pop(count)
-  # stacks[finish].push(temp).flatten!
+  keys.each_char.with_index do |c, idx|
+    if c != " "
+      key = c.to_i
+      stacks[key] = []
+      containers.each do |row|
+        if row[idx] != " "
+          stacks[key].push(row[idx])
+        end
+      end
+      stacks[key].reverse!
+    end
+  end
 
-  # Comment lines 30-31 out and 34-38 in for solution 1
-  while count > 0
-    temp = stacks[start].pop
-    stacks[finish].push(temp)
-    count -= 1
+  stacks
+end
+
+def move_one(stacks, moves)
+  moves.each do |move|
+    count = move[0]
+    start = move[1]
+    finish = move[2]
+
+    while count > 0
+      temp = stacks[start].pop
+      stacks[finish].push(temp)
+      count -= 1
+    end
+  end
+  stacks
+end
+
+def move_stack(stacks, moves)
+  moves.each do |move|
+    count = move[0]
+    start = move[1]
+    finish = move[2]
+  
+    temp = stacks[start].pop(count)
+    stacks[finish].push(temp).flatten!
+  end
+  stacks
+end
+
+def get_top_containers(stacks)
+  stacks.reduce("") do |top_crates, (key, stack)|
+    top_crates += stack.last
   end
 end
 
-top_crates = ""
+first_solution_stack = create_stacks(stack_key, stack_input)
+move_one(first_solution_stack, moves_input)
+puts "first solution: #{get_top_containers(first_solution_stack)}"
 
-stacks.each do |key, stack|
-  top_crates += stack.last
-end
-
-puts top_crates
+second_solution_stack = create_stacks(stack_key, stack_input)
+move_stack(second_solution_stack, moves_input)
+puts "second solution: #{get_top_containers(second_solution_stack)}"
